4213d2cfa045b7178d24cafdc2740f2791c9d8f7
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-11-03 09:53:43 +0700

    feat: init data

--- backend/lms/src/main/java/vn/uit/lms/core/repository/AccountRepository.java
+++ backend/lms/src/main/java/vn/uit/lms/core/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import vn.uit.lms.core.entity.Account;
+import vn.uit.lms.shared.constant.Role;
 
 import java.util.Optional;
 
@@ -12,6 +13,9 @@ public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpec
 
     Optional<Account> findOneByEmailIgnoreCase(String email);
     Optional<Account> findOneByUsername(String username);
-    Optional<Account> findByEmailOrUsername(String email, String username);
     Optional<Account> findByEmail(String email);
+    boolean existsByRole(Role role);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
 }
